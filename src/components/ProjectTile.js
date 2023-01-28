d08d00eabef7e404e79521aad3e7e8d93f7abefb
Edel Zamora <edelzamoragarcia@gmail.com>
2023-01-28 18:44:17 -0500

    Refactored project tile

--- src/components/ProjectTile.js
+++ src/components/ProjectTile.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import logoData from "../data_files/logosData";
 import SmallImageTile from "./SmallImageTile";
 import { useEffect, useRef } from "react";
+import ResumeDiagram from "./ResumeDiagram";
+import SiteDiagram from "./SiteDiagram";
+import PlaceholderDiagram from "./PlaceholderDiagram";
 
 export default function ProjectTile(props) {
   const [isDisplayed, setIsDisplayed] = useState(false);
@@ -23,26 +26,33 @@ export default function ProjectTile(props) {
     setIsDisplayed((prevState) => !prevState);
   }
 
+  let diagram = <PlaceholderDiagram />;
+  if (props.name === "Personal Website") {
+    diagram = <SiteDiagram />;
+  }
+  if (props.name === "Cloud Resume Challenge") {
+    diagram = <ResumeDiagram />;
+  }
+
   const techTiles = logoData.logos.map((ele) => {
     if (props.tech.includes(ele.item)) {
       return <SmallImageTile url={ele.url} title={ele.title} />;
     }
   });
 
+  const isAWSTilesRendered = props.aws.length
+    ? {
+        display: "block",
+      }
+    : {
+        display: "none",
+      };
+
   const awsTiles = logoData.aws.map((ele) => {
     if (props.aws.includes(ele.item)) {
       return (
-        <div style={{ marginTop: "10px" }}>
-          <img
-            style={{
-              width: "50px",
-              margin: "0 5px",
-              border: "solid 2px black",
-              borderRadius: "5px",
-            }}
-            src={ele.url}
-            alt={ele.item}
-          />
+        <div>
+          <img className="img-tile" src={ele.url} alt={ele.item} />
           <p
             style={{ fontSize: "10px", margin: "0px", fontWeight: "bold" }}
           >{`${ele.item}`}</p>
@@ -52,14 +62,7 @@ export default function ProjectTile(props) {
   });
 
   return (
-    <div
-      style={{
-        background: "#f2f1ef",
-        color: "black",
-        padding: "50px",
-        margin: "10px auto",
-      }}
-    >
+    <div className="project-tile-container">
       <h3 style={{ margin: "0" }}>{props.name}</h3>
 
       <div
@@ -68,90 +71,25 @@ export default function ProjectTile(props) {
       >
         <div ref={ref} className="modal">
           <div className="row">
-            <div
-              style={{
-                textAlign: "justify",
-                background: "lightblue",
-              }}
-              className="column"
-            >
-              <p className="description">{props.description}</p>
+            <div className="column">
+              <p className="description-text">{props.description}</p>
             </div>
-            <div
-              style={{
-                background: "orange",
-                width: "500px",
-                padding: "20px",
-                marginLeft: "10px",
-                marginRight: "10px",
-              }}
-            >
-              Diagram
-            </div>
+            <div className="diagram">{diagram}</div>
 
             <div className="column">
-              <div
-                className="row"
-                style={{
-                  background: "forestgreen",
-                  flexDirection: "column",
-                }}
-              >
-                <h4
-                  style={
-                    props.aws.length
-                      ? {
-                          margin: "0",
-                          display: "block",
-                          color: "#e2ae6c",
-                        }
-                      : { display: "none" }
-                  }
-                >
-                  AWS Services
-                </h4>
-                <div>{awsTiles}</div>
+              <div className="tiles-column" style={isAWSTilesRendered}>
+                <h4 className="small-heading">AWS Services</h4>
+                <div className="tiles">{awsTiles}</div>
               </div>
 
-              <div
-                className="row"
-                style={{
-                  background: "black",
-                  flexDirection: "column",
-                  margin: "0",
-                }}
-              >
-                <h4
-                  style={{
-                    color: "#e2ae6c",
-                    marginTop: "20px",
-                    marginBottom: "0",
-                  }}
-                >
-                  Tools & Tech
-                </h4>
-                <div
-                  style={{
-                    display: "flex",
-                    margin: "0 auto",
-                    flexWrap: "wrap",
-                  }}
-                >
-                  {techTiles}
-                </div>
+              <div className="tiles-column">
+                <h4 className="small-heading">Tools & Tech</h4>
+                <div className="tiles">{techTiles}</div>
               </div>
             </div>
           </div>
-          <div style={{ background: "red" }}>
-            <div
-              className="column"
-              style={{
-                background: "lightgreen",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "50px",
-              }}
-            >
+          <div>
+            <div className="overlay-btn-column">
               <a className="overlay-btn" onClick={handleBtn}>
                 Close
               </a>
