b12e4441cc4516952a93a795f8fd8a602337d931
edel zamora <edelzamora.tech@outlook.com>
2025-01-28 15:40:27 -0500

    Updated About

--- src/components/About.js
+++ src/components/About.js
@@ -19,15 +19,12 @@ export default function About() {
             fontSize: "60px",
           }}
         >
-          Edel Zamora Garcia
+          Edel Zamora
         </span>
       </h1>
-      <p>
-        I am a student at Western Governors University. I am currently getting
-        my Bachelor of Science in Cloud Computing and I am interested in cloud,
-        platform, devops engineering roles. Other roles I am interested in is
-        software development, software engineer and back-end engineering.
-      </p>
+      <p> I am a Cloud Engineer with a strong passion for security, specializing in Cloud Security. I plan to participate on the HackerOne platform as an independent security researcher, aiming to leverage my skills to identify and report vulnerabilities.</p>
+      <p> With experience at Amazon Web Services (AWS) as a Cloud Support Engineer on the security team, I have a strong foundation in cloud-native technologies. I bring expertise in cloud security, IAM, and automation. </p>
+      <p> AWS Services I helped support but not limited to KMS, IAM, ACM, Cognito, Organizations, CloudTrail, Secrets Manager.</p>
     </div>
   );
 }
